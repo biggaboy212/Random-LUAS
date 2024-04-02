1687e14c40532dcb3fc45c3b7573a73bb750a21f
biggaboy212 <75142294+biggaboy212@users.noreply.github.com>
2024-04-01 23:25:29 -0500

    Update and rename Newest Osiris Pro Source.lua to ss.lua

--- osiris source/ss.lua
+++ osiris source/ss.lua
@@ -1,4 +1,3 @@
---// 1
 -- Services
 local InputService, TeleportService, RunService, Workspace, Lighting, Players, HttpService, StarterGui, ReplicatedStorage  = game:GetService("UserInputService"), game:GetService("TeleportService"), game:GetService("RunService"), game:GetService("Workspace"), game:GetService("Lighting"), game:GetService("Players"), game:GetService("HttpService"), game:GetService("StarterGui"), game:GetService("ReplicatedStorage")
 -- Decendants 
